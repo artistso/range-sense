9417ec5e391ff4dbc66e4da7fb3916422d4fc642
Steven Owens <Soquarky@artistso.com>
2026-07-17 09:04:03 -0700

    Configure reproducible CycloneDX SBOM generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,17 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.cyclonedx.bom)
+}
+
+group = "com.soquarky"
+version = "0.2.0"
+
+tasks.cyclonedxBom {
+    projectType = "application"
+    componentName = "range-sense"
+    componentVersion = project.version.toString()
+    includeBomSerialNumber = false
+    includeLicenseText = false
+    includeBuildSystem = true
 }
